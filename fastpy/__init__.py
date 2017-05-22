7bdfd5d3ce6e39ebe00570dabd11be1b80c60e60
Mustafa <mguler@ucsd.edu>
2017-05-21 23:33:34 -0700

    got range

--- fastpy/__init__.py
+++ fastpy/__init__.py
@@ -1,14 +1,19 @@
 import sys
+import re
+import fasta
 
-def numseqs(file_name, qora):
+
+def qa(qora):
     qora = qora.lower()
     if qora == "a":
-        header = ">"
+        return ">"
     elif qora == "q":
-        header = "@"
+        return "@"
     else:
         sys.exit("must be q or a, you put %s" % qora)
 
+def numseqs(file_name, qora):
+    header = qa(qora)
     with open(file_name, "r") as f:
         count = 0
 
@@ -19,14 +24,7 @@ def numseqs(file_name, qora):
     return count
 
 def headers(file_name, qora):
-    qora = qora.lower()
-    if qora == "a":
-        header = ">"
-    elif qora = "q":
-        header = "@"
-    else:
-        sys.exit("must be q or a, you put %s" % qora)
-
+    header = qa(qora)
     with open(file_name, "r") as f:
         headers = []
 
@@ -49,5 +47,41 @@ def detect(file_name):
         else:
             return None
 
+def getrange(file_name, start, stop, qora, regex=None, hold_style=True):
+    result = {}
+    all_heads = []
+    header = qa(qora)
+    broken = False
+    if stop < start:
+        sys.exit("start must be less than stop")
 
+    with open(file_name, "r") as f:
+        count = 0
+        seq = ""
+        curr_head = None
+        for line in f:
+            if line.strip()[0] == header:
+                if count > stop:
+                    broken = True
+                    break
 
+                if curr_head and count >= start:
+                    result[curr_head] = seq.strip()
+                    all_heads.append(curr_head)
+
+                count += 1
+                curr_head = line.strip()
+                if regex:
+                    curr_head = re.search(regex, curr_head).group()
+
+                seq = ""
+            else:
+                seq += line.strip()
+                if hold_style:
+                    seq += "\n"
+
+    if not broken:
+        result[curr_head] = seq.strip()
+        all_heads.append(curr_head)
+
+    return result, all_heads
