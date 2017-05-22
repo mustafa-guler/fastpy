5a8e4eb30e5eef134ca81023dbaaa5ad73c23a38
Mustafa <mguler@ucsd.edu>
2017-05-21 21:59:21 -0700

    first

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(  name = "fastpy".
+        version = "1.0",
+        description = "fastx functions",
+        url = "https://github.com/mustafa-guler/fastpy",
+        author = "Mustafa Guler",
+        packages = ["fastpy"],
+        zip_safe = False)
+
