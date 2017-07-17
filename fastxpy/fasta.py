1525b7ae7af5d07a8c2c0779f8c8085f50476cf8
mustafa-guler <mguler@ucsd.edu>
2017-07-16 17:13:41 -0700

    python3 compatibility

--- fastxpy/fasta.py
+++ fastxpy/fasta.py
@@ -1,95 +1,118 @@
-def getseqs(file_name):
-    with open(file_name, "r") as f:
-        seqs =[]
-        seq = ""
-        f.readline()
+import builtins
 
-        for line in f:
-            line = line.strip()
-
-            if line[0] == ">":
-                seqs.append(seq)
-                seq = ""
-            else:
-                seq += line
-
-    return seqs
-
-def getheads(file_name):
-    with open(file_name, "r") as f:
-        headers= []
-
-        for line in f:
-            if line[0] == ">":
-                headers.append(line.strip())
-
-    return headers
-
-class fa(file):
-    def __init__(self, name):
-        self._lastheader = 0
+class fa(object):
+    def __init__(self, file_name, mode="r", fasta_mode="lines"):
+        self._fasta_file = builtins.open(file_name, mode, encoding="utf-8")
+        if not fasta_mode in ["seqs", "heads", "lines"]:
+            raise ValueError("fasta_mode parameter must be either lines, seqs or heads")
+        self._fasta_mode = fasta_mode
         self._offset = 0
-        super(fa, self).__init__(name)
+        self._last_header = 0
+        self._recent_header = False
+
+    def setfastamode(self, fasta_mode):
+        if not fasta_mode in ["seqs", "heads", "lines"]:
+            raise ValueError("fasta_mode parameter must be either lines, seqs or heads")
+        self._fasta_mode = fasta_mode
+
+    def __iter__(self):
+        if self._fasta_mode == "lines":
+            unit = self.readline()
+        elif self._fasta_mode == "heads":
+            unit = self.readheader()
+        else:
+            unit = self.readseq()
+
+        while unit:
+            yield unit
+            if self._fasta_mode == "lines":
+                unit = self.readline()
+            elif self._fasta_mode == "heads":
+                unit = self.readheader()
+            else:
+                unit = self.readseq()
 
     def readline(self):
-        line = super(fa, self).readline()
-
-        if line and line[0] == ">":
-            self._lastheader += self._offset
-            self._offset = 0
+        line = self._fasta_file.readline()
+        if line:
+            if line[0] == ">":
+                self._last_header += self._offset
+                self._offset = 0
+                self._recent_header = True
+            else:
+                self._recent_header = False
 
         self._offset += len(line)
 
-        return line
+        return str(line)
 
-    def readheader(self):
-        for line in self:
-            if line[0] == ">":
+    def readseq(self):
+        while not self._recent_header:
+            line = self.readline()
+            if not line:
                 return line
-        return None
 
-    def prevheader(self):
-        prev = self.tell()
-        self.seek(self._lastheader)
-        header = self.readline()
-        self.seek(prev)
-        return header
-
-    def gotoprevheader(self):
-        self.seek(self._lastheader)
-        return self.readline()
-
-    def readseq(self, keepstyle=True, processing=False):
-        head = self.readline()
-
-        if not head:
-            return None
-
-        if not head[0] == ">":
-            for head in self:
-                if not head:
-                    return None
-                if head[0] == ">":
-                    break
-
-
-        prev = self.tell()
-        line = self.readline()
         seq = ""
-        while line and line[0] != ">":
-            if not keepstyle:
-                line = line.strip()
 
-            seq += line
-            prev = self.tell()
+        line = self.readline()
+        while line: 
+            if self._recent_header:
+                self._fasta_file.seek(self._last_header)
+                self._offset = 0
+                self._recent_header = False
+                break
+
+            seq += line.strip()
             line = self.readline()
 
-        self.seek(prev)
+        return seq
 
-        if processing:
-            return head.strip(), seq.strip()
 
-        else:
-            result = head + seq
-            return result
+    def readseqs(self):
+        seqs = []
+        seq = self.readseq()
+        while seq:
+            seqs.append(seq)
+            seq = self.readseq()
 
+        return seqs
+
+    def readheader(self):
+        line = self.readline()
+
+        while line:
+            if line[0] == ">":
+                return line.strip()
+            line = self.readline()
+        
+        return line.strip()
+
+    def readheaders(self):
+        heads = []
+        head = self.readheader()
+        while head:
+            heads.append(head)
+            head = self.readheader()
+
+        return heads
+
+    def prevheader(self):
+        prev = self._fasta_file.tell()
+        self._fasta_file.seek(self._last_header)
+        header = self._fasta_file.readline()
+        self._fasta_file.seek(prev)
+        return header.strip()
+
+    def close(self):
+        self._fasta_file.close()
+
+
+class open(fa):
+    def __init__(self, file_name, mode="r", fasta_mode="seqs"):
+        super(self.__class__, self).__init__(file_name, mode, fasta_mode)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, excep_type, excep_val, trace):
+        super(self.__class__, self).close()
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(  name = "fastxpy",
-        version = "1.0.0a7",
-        description = "fastx functions",
+        version = "1.0.0a8",
+        description = "fasta and fastq file functions",
         url = "https://github.com/mustafa-guler/fastpy",
         author = "Mustafa Guler",
         packages = ["fastxpy"],
